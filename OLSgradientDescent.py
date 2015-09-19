4411f67dd929269df79b7799466c00744bbd9c6a
Alexandre Piche <AlexPiche@web097032.wireless.mcgill.ca>
2015-09-19 16:02:11 -0400

    functional gradient descent

--- OLSgradientDescent.py
+++ OLSgradientDescent.py
@@ -6,12 +6,24 @@ def gradientDescent(Y, X, eps, nb_iter, alpha):
     epsilon = 9999
     i = 1
     W = np.random.normal(0, 1, X.shape[1])
+
     while (epsilon > eps and i < nb_iter):
-        W_new = W - 2 * alpha * (np.dot(np.dot(X.T, X), W) - np.dot(X.T, Y))
+        hypothesis = np.dot(X, W)
+        loss = hypothesis - Y
+        gradient = 2 * np.dot(X.T, loss)
+        W_new = W - alpha * gradient / X.shape[0]
         epsilon = np.linalg.norm(W_new - W, ord = 1)
         i += 1
         W = W_new
 
     return W
 
+
+nInstances = 1000
+nParams = 10
+trueWeights = np.asarray(range(nParams))*2
+x = np.random.rand(nInstances,nParams)
+y = np.dot(x,trueWeights)
+
+test = gradientDescent(y,x,0.01, 1000, 0.01)
 
