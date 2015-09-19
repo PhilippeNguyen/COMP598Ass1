c8b3dc78c9c0bc33ed0e30aa2029b21333dde12f
Alexandre Piche <AlexPiche@web112251.wireless.mcgill.ca>
2015-09-19 15:13:55 -0400

    gradient descent code

--- OLSgradientDescent.py
+++ OLSgradientDescent.py
@@ -1,7 +1,17 @@
 import numpy as np
+import pdb
 
-def gradientDescent( eps, nb_iter):
+def gradientDescent(Y, X, eps, nb_iter, alpha):
 
-    w[0] = what
-    while (epsilon < eps or i < nb_iter):
-        w[i] = w[i-1] - 2 * alpha ( X X w[i-1] - X Y)
+    epsilon = 9999
+    i = 1
+    W = np.random.normal(0, 1, X.shape[1])
+    while (epsilon > eps and i < nb_iter):
+        W_new = W - 2 * alpha * (np.dot(np.dot(X.T, X), W) - np.dot(X.T, Y))
+        epsilon = np.linalg.norm(W_new - W, ord = 1)
+        i += 1
+        W = W_new
+
+    return W
+
+
