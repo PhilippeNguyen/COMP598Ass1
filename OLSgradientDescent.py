4d8e4a3f849b3522902f75eb0d479c69545e9f46
Alexandre Piche <AlexPiche@web097032.wireless.mcgill.ca>
2015-09-19 16:17:30 -0400

    implement ridge regressions

--- OLSgradientDescent.py
+++ OLSgradientDescent.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pdb
 
-def gradientDescent(Y, X, eps, nb_iter, alpha):
+def gradientDescent(Y, X, eps, nb_iter, alpha, penalty, ll = 0):
 
     epsilon = 9999
     i = 1
@@ -10,6 +10,10 @@ def gradientDescent(Y, X, eps, nb_iter, alpha):
     while (epsilon > eps and i < nb_iter):
         hypothesis = np.dot(X, W)
         loss = hypothesis - Y
+
+        if(penalty == 'ridge'):
+            loss += 2 * ll * np.linalg.norm(W, ord = 1)
+
         gradient = 2 * np.dot(X.T, loss)
         W_new = W - alpha * gradient / X.shape[0]
         epsilon = np.linalg.norm(W_new - W, ord = 1)
@@ -25,5 +29,5 @@ trueWeights = np.asarray(range(nParams))*2
 x = np.random.rand(nInstances,nParams)
 y = np.dot(x,trueWeights)
 
-test = gradientDescent(y,x,0.01, 1000, 0.01)
+test = gradientDescent(y,x,0.0001, 10000, 0.01, 'ridge', 0.01)
 
