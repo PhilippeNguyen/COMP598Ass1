5c8c951c73ce9fe36ac1a352abf94fd099d57921
yashLakhani <yashlakhani13@gmail.com>
2015-09-30 18:21:17 -0400

    Increased speed, bug fixes, entity extraction not working

--- data_extractor.py
+++ data_extractor.py
@@ -1,12 +1,12 @@
 __author__ = 'yaSh'
 
 url_list = []
+corpus = {}
 import re
 import urllib2
 from BeautifulSoup import BeautifulSoup
 from Queue import Queue, Empty
 from threading import Thread
-
 
 visited = set()
 queue = Queue()
@@ -14,10 +14,36 @@ queue = Queue()
 def get_text(url):
     from boilerpipe.extract import Extractor
     try :
-        extractor = Extractor(extractor='ArticleExtractor', url=url)
+        extractor = Extractor(extractor='DefaultExtractor', url=url)
         return extractor.getText(), extractor.getHTML()
     except:
         return "",""
+
+def get_title(html):
+    try:
+        titleRE = re.compile(r"<H1 class=\"title\">(.+?)</H1>")
+        title = titleRE.search(html).group(1)
+    except: title = ""
+    return title
+
+def get_topic(html):
+    try:
+        topicRE = re.compile(r"<LABEL for=\"vertical-brief\">(.+?)</LABEL>")
+        topic = topicRE.search(html).group(1)
+    except: topic = "Other"
+    return topic
+
+import json
+def get_num_likes(url):
+     try:
+         share_link = "http://graph.facebook.com/?id="+url
+         response = urllib2.urlopen(share_link)
+         data = json.loads(response.read())
+         shares =  dict(data)['shares']
+     except:
+         shares = 0
+     return shares
+
 
 def link_crawl(root, article_limit):
     name = root.rsplit(".")[1]
@@ -43,14 +69,13 @@ def link_crawl(root, article_limit):
                             queue.put(href)
                             date =re.search(r'(\d+/\d+/\d+)', href).group(1)
                             text, html = get_text(href)
-                            try:
-                                titleRE = re.compile(r"<H1 class=\"title\">(.+?)</H1>")
-                                title = titleRE.search(html).group(1)
-                            except:
-                                title = ""
+                            title = get_title(html)
+                            topic = get_topic(html)
+                            likes = get_num_likes(href)
                             if len(title) & len(text):
-                                feature_vector = {'url':href, 'title':title, 'content': text, 'date':date}
-                                print (feature_vector['title'] , feature_vector['date'])
+                                feature_vector = {'url':href, 'title':title, 'content': text, 'date':date, 'topic':topic, 'likes':likes}
+                                corpus[title] = text
+                                print (title, date, topic, likes)
                                 if date:
                                     url_list.append(feature_vector)
         except Empty:
@@ -58,9 +83,9 @@ def link_crawl(root, article_limit):
     return parse
 
 def get_data(limit):
+    corpus = []
     import time
     start = time.time()
-
     root = "http://www.huffingtonpost.ca/"
     parser = link_crawl(root, article_limit=limit)
     queue.put(root)
@@ -73,7 +98,7 @@ def get_data(limit):
         worker.join()
     end = time.time()
     print
-    print "Time Taken to Extract Data : " + str(end - start) + " seconds"
+    print "Time Taken to Extract Data : " + (str((end - start)/60.0)) + " minutes"
     print
     return url_list
 
