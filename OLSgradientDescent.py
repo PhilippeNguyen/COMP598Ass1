5f9ad2186c47c82ae5440bcb42390dd96caa2467
Alexandre Piche <AlexPiche@wpa181006.wireless.mcgill.ca>
2015-09-30 13:05:32 -0400

    update bibliography

--- OLSgradientDescent.py
+++ OLSgradientDescent.py
@@ -12,13 +12,14 @@ def getSSR(Y, X, W):
 
 def logLik(Y, X, W):
     SSR = getSSR(Y, X, W)
-    sigma2 = SSR / (X[0]-2)
-    myLogLik = np.log(1/np.sqrt(np.abs(2*np.pi*sigma2))) - 1/(2 * sigma2) * SSR
+    sigma2 = SSR / (X.shape[0]-2)
+    myLogLik = -0.5 * X.shape[0] * np.log(2*np.pi) - 0.5 * X.shape[0] * np.log(sigma2) - SSR/(2*sigma2)
     return myLogLik
 
 def getSDBetas(Y, X, W):
     sigma2 = getSigma2(Y, X, W)
-    return np.sqrt(np.abs(sigma2 / np.dot(X.T, X)))
+    toRet = np.sqrt(np.abs(sigma2 / np.dot(X.T, X)))
+    return np.diag(toRet)
 
 def getSignificance(Y, X, W):
     SD = getSDBetas(Y, X, W)
@@ -26,20 +27,32 @@ def getSignificance(Y, X, W):
 
 def getAdjustedR2(Y, X, W):
     Ymean = np.mean(Y)
-    R2 = np.linalg.norm(np.dot(X, W) - Ymean, ord=2)/np.linalg.norm(Y - Ymean, ord=2) 
-    adjustedR2 = 1 - ((1 - R2) * (X.shape[0]-1)/(X.shape[0]-X.shape[1]-1))
-    return adjustedR2
+    mySSR = getSSR(Y, X, W)
+    #R2 = np.linalg.norm(np.dot(X, W) - Ymean, ord=2)/np.linalg.norm(Y - Ymean, ord=2) 
+    #adjustedR2 = 1 - ((1 - R2) * (X.shape[0]-1)/(X.shape[0]-X.shape[1]-1))
+    num = 1/(X.shape[0] - X.shape[1]) * np.sum(mySSR)
+    denom = 1.0/(X.shape[0]-1) * np.linalg.norm(Y - Ymean, ord = 2)
+    return 1-( num/denom )
 
 
 def getAIC(Y, X, W):
     myLogLik = logLik(Y, X, W)
-    return 2 * X.shape[1] - 2 * myLogLik
+    return myLogLik - X.shape[1]
 
 def getBIC(Y, X, W):
     myLogLik = logLik(Y, X, W)
-    return X.shape[1] * np.log (X.shape[0]) - 2 * myLogLik
+    return myLogLik - 0.5 * X.shape[1] * np.log (X.shape[0])
     
     
+def getModelInfo(Y, X, W):
+    W_significance = getSignificance(Y, X, W)
+    r2 = getAdjustedR2(Y, X, W)
+    aic = getAIC(Y, X, W)
+    bic = getBIC(Y, X, W)
+    myLogLik = logLik(Y, X, W)
+    toRet = {'Weights': W, 'Significance': W_significance, 'Adjusted R2': r2, 'AIC': aic, 'BIC': bic, 'Log Likelihood': myLogLik}
+    return toRet
+
 def gradientDescent(Y, X, tolerance, nb_iterations, learning_rate, ridge = False, penalty_rate = 0):
 
     epsilon = 9999
@@ -59,13 +72,18 @@ def gradientDescent(Y, X, tolerance, nb_iterations, learning_rate, ridge = False
         epsilon = np.linalg.norm(W_new - W, ord = 1) / X.shape[1]
         i += 1
         W = W_new
+    toRet = getModelInfo(Y, X, W)
+    return toRet
 
-    return W
-
-###W_significance = getSignificance(Y, X, W)
-###r2 = getAdjustedR2(Y, X, W)
-###aic = getAIC(Y, X, W)
-###bic = getBIC(Y, X, W)
-###toRet = {'Weights': W, 'Significance': W_significance, 'Adjusted R2': r2, 'AIC': aic, 'BIC': bic}
-##return W
-
+#nInstances = 500
+#nParams = 10
+#noiseLevel = 0
+#trueWeights = np.asarray(range(nParams))
+##    trueWeights[5:] = 0
+##    trueWeights[:4] = 0
+#X = np.random.rand(nInstances,nParams)
+#Y = np.dot(X,trueWeights) + noiseLevel*np.random.rand(np.size(nInstances))
+#Yfitted = np.dot(X,test['Weights'])
+#
+#test = gradientDescent(Y, X, 0, 10000, 0.01)
+#
--- pcatest.py
+++ pcatest.py
@@ -6,6 +6,20 @@ from sklearn import linear_model
 from OLSgradientDescent import gradientDescent
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn import linear_model
+
+
+nInstances = 500
+nParams = 100
+noiseLevel = 500
+trueWeights = np.asarray(range(nParams))
+#    trueWeights[5:] = 0
+#    trueWeights[:4] = 0
+X = np.random.rand(nInstances,nParams)
+Y = np.dot(X,trueWeights) + noiseLevel*np.random.rand(np.size(nInstances))
+
+test = gradientDescent(Y, X, 0, 1000, 0.01)
+
 
 input = pd.read_csv('OnlineNewsPopularity/OnlineNewsPopularity.csv')
 
@@ -13,13 +27,19 @@ inp = input.as_matrix()
 
 X = inp[:,1:60]
 Y = inp[:,60]
+Y = Y.reshape(Y.shape[0],1)
+X, Y = shuffle(X,Y)
 pca = PCA(n_components = 3)
 
 pca.fit(X)
 print(pca.explained_variance_ratio_) 
 X_new = pca.transform(X)
-X_new = X_new 
-
 ones = np.ones(X_new.shape[0])
 X_new = np.column_stack((ones, X_new))
-test = gradientDescent(Y[0:1000,], X_new[0:1000,], 0, 2000000, 1e-13)
+
+clf = linear_model.LinearRegression()
+clf.fit(X_new[0:30000,], Y[0:30000,])
+clf.fit(X, Y)
+
+clf.predict(X[30000:,])
+
